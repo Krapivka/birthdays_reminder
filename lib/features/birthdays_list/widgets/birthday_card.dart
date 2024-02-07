47676966523b6889e67d973d88a592f39d9d7f76
Ivan Krapivchenkov <ivan.krapivchenkov@mail.ru>
2024-02-07 08:11:08 +0300

    added a transition to the update page

--- lib/features/birthdays_list/widgets/birthday_card.dart
+++ lib/features/birthdays_list/widgets/birthday_card.dart
@@ -1,8 +1,10 @@
 import 'dart:io';
 
+import 'package:auto_route/auto_route.dart';
 import 'package:birthdays_reminder/core/data/models/person_model.dart';
 import 'package:birthdays_reminder/core/utils/datetime.dart';
 import 'package:birthdays_reminder/features/birthdays_list/bloc/birthdays_list_bloc.dart';
+import 'package:birthdays_reminder/router/router.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -18,41 +20,46 @@ class BirthdayCard extends StatelessWidget {
   Widget build(BuildContext context) {
     final bloc = BlocProvider.of<BirthdaysListBloc>(context);
     final file = File(person.filePath);
-    return Dismissible(
-      key: UniqueKey(),
-      background: const Card(
-        color: Color.fromARGB(255, 253, 253, 253),
-        child: Icon(Icons.delete),
-      ),
-      onDismissed: (direction) {
-        bloc.add(DeletePersonBirthdaysListEvent(id: person.id));
+    return InkWell(
+      onTap: () {
+        AutoRouter.of(context).push(UpdateBirthdayRoute(personmodel: person));
       },
-      child: Container(
-        decoration: BoxDecoration(borderRadius: BorderRadius.circular(8)),
-        child: Card(
-            child: ListTile(
-                leading: CircleAvatar(
-                  radius: 28,
-                  backgroundImage: person.filePath == "/"
-                      ? const AssetImage("assets/images/avatar.png")
-                      : FileImage(file) as ImageProvider,
-                ),
-                title: Text(
-                  person.name,
-                  maxLines: 1,
-                ),
-                subtitle: Text(DateTimeUtils().formatDate(person.birthdate)),
-                trailing: Container(
-                    height: 30,
-                    width: 30,
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      border: Border.all(width: 2),
-                    ),
-                    child: Center(
-                        child: Text(DateTimeUtils()
-                            .getDifferenceCurrentDayBirthDay(
-                                person.birthdate)))))),
+      child: Dismissible(
+        key: UniqueKey(),
+        background: const Card(
+          color: Color.fromARGB(255, 253, 253, 253),
+          child: Icon(Icons.delete),
+        ),
+        onDismissed: (direction) {
+          bloc.add(DeletePersonBirthdaysListEvent(id: person.id));
+        },
+        child: Container(
+          decoration: BoxDecoration(borderRadius: BorderRadius.circular(8)),
+          child: Card(
+              child: ListTile(
+                  leading: CircleAvatar(
+                    radius: 28,
+                    backgroundImage: person.filePath == "/"
+                        ? const AssetImage("assets/images/avatar.png")
+                        : FileImage(file) as ImageProvider,
+                  ),
+                  title: Text(
+                    person.name,
+                    maxLines: 1,
+                  ),
+                  subtitle: Text(DateTimeUtils().formatDate(person.birthdate)),
+                  trailing: Container(
+                      height: 30,
+                      width: 30,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        border: Border.all(width: 2),
+                      ),
+                      child: Center(
+                          child: Text(DateTimeUtils()
+                              .getDifferenceCurrentDayBirthDay(
+                                  person.birthdate)))))),
+        ),
       ),
     );
   }
